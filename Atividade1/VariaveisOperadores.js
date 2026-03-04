98a53e34decbca42a47f0ac2bccbfa19617c1402
alberthcavalcanti <alberthdc5@gmail.com>
2026-03-04 18:46:48 -0300

    Primeira atividaede de Programação Web.

---
+++ Atividade1/VariaveisOperadores.js
@@ -0,0 +1,16 @@
+let primeiroNumero;
+let segundoNumero;
+let resultado; 
+
+let soma = primeiroNumero + segundoNumero;
+let subtracao = primeiroNumero - segundoNumero;
+let multiplicacao = primeiroNumero * segundoNumero;
+let divisao = primeiroNumero / segundoNumero;
+
+primeiroNumero = prompt("Digite o primeiro número:");
+segundoNumero = prompt("Digite o segundo número:");
+
+console.log(`A soma do ${primeiroNumero} e ${segundoNumero} é: ${soma}`);
+console.log(`A subtração do ${primeiroNumero} e ${segundoNumero} é: ${subtracao}`);
+console.log(`A multiplicação do ${primeiroNumero} e ${segundoNumero} é: ${multiplicacao}`);
+console.log(`A divisão do ${primeiroNumero} e ${segundoNumero} é: ${divisao}`);
